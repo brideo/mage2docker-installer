595a700fadc4e648a927e182481bb9d2f3e5e8c3
Nathan McBride <nathan@brideo.co.uk>
2016-02-04 23:13:24 +0000

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+#Magento 2 Installer
+
+So this is currently a wrapper around [MageInferno's docker setup](https://mageinferno.com/blog/magento-2-development-docker-os-x) for OS.
+
+This uses `docker`, `docker-compose`, `virtualbox`, `vagrant` and `dinghy`.
+
+To install these requirements:
+
+    brew update
+    brew install docker
+    brew install docker-compose
+    brew cask install virtualbox
+    brew cask install vagrant
+    brew install https://github.com/codekitchen/dinghy/raw/latest/dinghy.rb
+
+
---
+++ src/NewCommand.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Brideo\Installer\Console;
+
+use RuntimeException;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Dumper;
+use Symfony\Component\Yaml\Parser;
+
+class NewCommand extends Command
+{
+    /**
+     * Configure the command options.
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->setName('new')
+            ->setDescription('Create a new Magento 2 install on docker.')
+            ->addArgument('name', InputArgument::REQUIRED);
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->validate($input);
+        $output->writeln('<info>Starting Your New Magento 2 Install...</info>');
+        $this->createDockerComposeYml($input);
+        $this->createGitIgnore($input);
+        $this->runTheInstall($input);
+    }
+
+
+    /**
+     * @param InputInterface $input
+     *
+     * @return string
+     */
+    protected function getDirectoryPath(InputInterface $input)
+    {
+        return getcwd() . '/' . $input->getArgument('name');
+    }
+
+    /**
+     * Run validation checks.
+     *
+     * @param InputInterface $input
+     * @throws RuntimeException
+     */
+    protected function validate(InputInterface $input)
+    {
+        if (is_dir($this->getDirectoryPath($input))) {
+            throw new RuntimeException("You already have a directory at {$this->getDirectoryPath($input)}");
+        }
+
+    }
+
+    /**
+     * Worse function in the world, get a yaml file, convert it,
+     * update it, convert it back then create it in the new project.
+     *
+     * @param InputInterface $input
+     *
+     * @return string
+     */
+    protected function getDockerComposeFileContents(InputInterface $input)
+    {
+        $dockerCompose = file_get_contents(__DIR__ . '/templates/docker-compose.yml');
+        $yaml = new Parser();
+        $parsedYaml = $yaml->parse($dockerCompose);
+
+        //pure unadulterated filth, sorry.
+        foreach($parsedYaml['app'] as $key => $data) {
+            $parsedYaml['app'][$key] = str_replace('mysite', $input->getArgument('name'), $data);
+        }
+        foreach($parsedYaml['setup']['environment'] as $key => $data) {
+            $parsedYaml['setup']['environment'][$key] = str_replace('mysite', $input->getArgument('name'), $data);
+        }
+
+        $dumper = new Dumper();
+        return $dumper->dump($parsedYaml, 3, false, true);
+    }
+
+    /**
+     * @param InputInterface $input
+     *
+     * @return $this
+     */
+    protected function createDockerComposeYml(InputInterface $input)
+    {
+        mkdir($this->getDirectoryPath($input));
+
+        file_put_contents(
+            $this->getDirectoryPath($input). '/docker-compose.yml',
+            $this->getDockerComposeFileContents($input)
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param InputInterface $input
+     *
+     * @return $this
+     */
+    protected function createGitIgnore(InputInterface $input)
+    {
+        file_put_contents(
+            $this->getDirectoryPath($input). '/.gitignore',
+            file_get_contents(__DIR__ . '/templates/.gitignore')
+        );
+
+        return $this;
+    }
+
+    /**
+     * @param InputInterface $input
+     */
+    protected function runTheInstall(InputInterface $input)
+    {
+        exec("cd {$input->getArgument('name')} && docker-compose up setup");
+    }
+
+}
